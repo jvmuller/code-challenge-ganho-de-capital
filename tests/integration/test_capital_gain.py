db380844d4fdc90df6d3e290615dda4394983b99
João Vitor Müller <muller.joaovitor00@gmail.com>
2025-04-17 15:51:26 -0300

    refactor: ajusta a formatação das saídas esperadas nos testes de integração para incluir quebras de linha

--- tests/integration/test_capital_gain.py
+++ tests/integration/test_capital_gain.py
@@ -10,28 +10,31 @@ from src.main import main
 
 def load_input_file(filename):
     with open(os.path.join("input_examples", filename), "r") as f:
-        return json.loads(f.read())
+        return f.read()
 
 
 @pytest.mark.parametrize(
     "input_file,expected_output",
     [
-        ("input_01.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]'),
-        ("input_02.txt", '[{"tax": 0.0},{"tax": 10000.0},{"tax": 0.0}]'),
-        # (
-        #     "input_01_with_02.txt",
-        #     '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}][{"tax": 0.0},{"tax": 10000.0},{"tax": 0.0}]',
-        # ),
-        ("input_03.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 1000.0}]'),
-        ("input_04.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]'),
-        ("input_05.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 10000.0}]'),
-        ("input_06.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 3000.0}]'),
+        ("input_01.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]\n'),
+        ("input_02.txt", '[{"tax": 0.0},{"tax": 10000.0},{"tax": 0.0}]\n'),
+        (
+            "input_01_with_02.txt",
+            '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]\n[{"tax": 0.0},{"tax": 10000.0},{"tax": 0.0}]\n',
+        ),
+        ("input_03.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 1000.0}]\n'),
+        ("input_04.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0}]\n'),
+        ("input_05.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 10000.0}]\n'),
+        ("input_06.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 3000.0}]\n'),
         (
             "input_07.txt",
-            '[{"tax": 0.0}, {"tax": 0.0}, {"tax": 0.0}, {"tax": 0.0}, {"tax": 3000.0},{"tax": 0.0}, {"tax": 0.0}, {"tax": 3700.0}, {"tax": 0.0}]',
+            '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 3000.0},{"tax": 0.0},{"tax": 0.0},{"tax": 3700.0},{"tax": 0.0}]\n',
         ),
-        ("input_08.txt", '[{"tax": 0.0},{"tax": 80000.0},{"tax": 0.0},{"tax": 60000.0}]'),
-        # ("input_09.txt", '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 1000.0},'), # TODO: Faltando valores no output
+        ("input_08.txt", '[{"tax": 0.0},{"tax": 80000.0},{"tax": 0.0},{"tax": 60000.0}]\n'),
+        (
+            "input_09.txt",
+            '[{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 0.0},{"tax": 1000.0},{"tax": 2400.0}]\n',
+        ),
     ],
 )
 def test_main_with_input(input_file, expected_output):
@@ -39,7 +42,7 @@ def test_main_with_input(input_file, expected_output):
     input_data = load_input_file(input_file)
     output_data = io.StringIO()
 
-    with patch("sys.stdin", io.StringIO(json.dumps(input_data))), patch("sys.stdout", output_data):
+    with patch("sys.stdin", io.StringIO(input_data)), patch("sys.stdout", output_data):
         main()
 
     assert output_data.getvalue() == expected_output
